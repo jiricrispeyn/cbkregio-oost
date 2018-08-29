138e6ababac770e8245ab307eb365b130db55f77
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-08-30 01:01:34 +0200

    Ignore remote debugger YellowBox warning

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationActions } from "react-navigation";
-import createBottomTabNavigator from "./src/navigator";
+import { StyleSheet, YellowBox } from "react-native";
+import createBottomTabNavigator from "./src/services/navigator";
 
+YellowBox.ignoreWarnings(["Remote debugger"]);
 const AppNavigator = createBottomTabNavigator;
 
 export default class App extends React.Component {
