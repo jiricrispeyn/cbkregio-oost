72e5ec55b5942279fbb52ba6ae8c258b4c300b76
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-08-31 00:43:00 +0200

    Use prettier single quotes

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { StyleSheet, YellowBox } from "react-native";
-import createBottomTabNavigator from "./src/services/navigator";
+import React from 'react';
+import { StyleSheet, YellowBox } from 'react-native';
+import createBottomTabNavigator from './src/services/navigator';
 
-YellowBox.ignoreWarnings(["Remote debugger"]);
+YellowBox.ignoreWarnings(['Remote debugger']);
 const AppNavigator = createBottomTabNavigator;
 
 export default class App extends React.Component {
   render() {
-    return <AppNavigator />;
+    return <AppNavigator style={styles.container} />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
