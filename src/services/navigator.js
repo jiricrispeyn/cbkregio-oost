b44340dbb6d337ffe70012aa280d085988b1f01c
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-08-31 00:43:48 +0200

    Style stack navigator header

--- src/services/navigator.js
+++ src/services/navigator.js
@@ -1,16 +1,36 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View } from 'react-native';
 import {
   createBottomTabNavigator,
   createStackNavigator
-} from "react-navigation";
-import LeaguesScreen from "../screens/LeaguesScreen";
-import LeagueDetailScreen from "../screens/LeagueDetailScreen";
+} from 'react-navigation';
+import LeaguesScreen from '../screens/LeaguesScreen';
+import LeagueDetailScreen from '../screens/LeagueDetailScreen';
+
+const headerStyle = {
+  height: 64,
+  backgroundColor: '#6563A4',
+  shadowColor: 'transparent'
+};
+const headerTitleStyle = {
+  color: '#fff',
+  fontSize: 17
+};
+const headerBackTitleStyle = {
+  color: '#fff'
+};
+const headerTintColor = '#fff';
+const styles = {
+  headerStyle,
+  headerTitleStyle,
+  headerBackTitleStyle,
+  headerTintColor
+};
 
 class SettingsScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Settings!</Text>
       </View>
     );
@@ -22,18 +42,20 @@ const LeaguesStackNavigator = createStackNavigator(
     Leagues: {
       screen: LeaguesScreen,
       navigationOptions: {
-        title: "Reeksen"
+        title: 'Reeksen',
+        ...styles
       }
     },
     LeagueDetail: {
       screen: LeagueDetailScreen,
       navigationOptions: ({ navigation }) => ({
-        title: navigation.state.params.league
+        title: navigation.state.params.league,
+        ...styles
       })
     }
   },
   {
-    initialRouteName: "Leagues"
+    initialRouteName: 'Leagues'
   }
 );
 
