7744e9e54da6cc161081c9ed3bc87b806c5d9587
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-09-05 01:41:15 +0200

    Extend ListItem with rightTitle and subtitle

--- src/components/list/ListItem.js
+++ src/components/list/ListItem.js
@@ -1,15 +1,19 @@
-import React, { PureComponent } from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import React, { PureComponent } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 class ListItem extends PureComponent {
   state = {};
   render() {
-    const { title, divider, onPress } = this.props;
+    const { title, rightTitle, subtitle, divider, onPress } = this.props;
     return (
       <View>
         {divider && <View style={styles.divider} />}
         <TouchableOpacity style={styles.listItem} onPress={onPress}>
-          <Text style={styles.title}>{title}</Text>
+          <View style={styles.titleWrapper}>
+            <Text style={styles.title}>{title}</Text>
+            {rightTitle && <Text style={styles.rightTitle}>{rightTitle}</Text>}
+          </View>
+          {subtitle && <Text style={styles.subtitle}>{subtitle}</Text>}
         </TouchableOpacity>
       </View>
     );
@@ -20,13 +24,27 @@ const styles = StyleSheet.create({
   listItem: {
     padding: 30
   },
+  titleWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
   title: {
     fontSize: 16,
-    color: "#1D1D26"
+    color: '#1D1D26'
+  },
+  rightTitle: {
+    fontSize: 13,
+    color: 'rgba(29, 29, 38, .5)'
+  },
+  subtitle: {
+    marginTop: 5,
+    fontSize: 13,
+    color: 'rgba(29, 29, 38, .5)'
   },
   divider: {
     height: 1,
-    backgroundColor: "rgba(29, 29, 38, .1)"
+    backgroundColor: 'rgba(29, 29, 38, .1)'
   }
 });
 
