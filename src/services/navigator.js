7a0138e787edb2468e522b6e0b566de5c56b587f
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-10-02 01:37:39 +0200

    Implement custom header into navigator

--- src/services/navigator.js
+++ src/services/navigator.js
@@ -1,43 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import {
-  createBottomTabNavigator,
-  createStackNavigator
-} from 'react-navigation';
+import { View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import CustomHeader from '../components/header/CustomHeader';
 import LeaguesScreen from '../screens/LeaguesScreen';
 import LeagueDetailScreen from '../screens/LeagueDetailScreen';
-
-const headerStyle = {
-  height: 64,
-  backgroundColor: '#172E4C',
-  borderBottomWidth: 0
-};
-const headerTitleStyle = {
-  color: '#fff',
-  fontSize: 34,
-  fontWeight: '600'
-};
-const headerBackTitleStyle = {
-  color: '#fff',
-  fontSize: 15
-};
-const headerTintColor = '#fff';
-const styles = {
-  headerStyle,
-  headerTitleStyle,
-  headerBackTitleStyle,
-  headerTintColor
-};
-
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
 
 export default createStackNavigator(
   {
@@ -45,14 +11,14 @@ export default createStackNavigator(
       screen: LeaguesScreen,
       navigationOptions: {
         title: 'Reeksen',
-        ...styles
+        header: props => <CustomHeader {...props} />
       }
     },
     LeagueDetail: {
       screen: LeagueDetailScreen,
       navigationOptions: ({ navigation }) => ({
         title: navigation.state.params.league,
-        ...styles
+        header: props => <CustomHeader {...props} />
       })
     }
   },
