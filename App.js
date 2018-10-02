20c972a8bb8d446231bafcc070091ef00fcc9c98
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-10-03 00:55:35 +0200

    Let prettier add trailing commas

--- App.js
+++ App.js
@@ -19,6 +19,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     height: '100%',
-    backgroundColor: '#172E4C'
-  }
+    backgroundColor: '#172E4C',
+  },
 });
