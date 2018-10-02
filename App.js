ec58932a4edb8cec1e9e6c7e4d003de9d3d995be
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-10-03 00:52:41 +0200

    Add SafeAreaView and StatusBar to App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, YellowBox } from 'react-native';
+import { StyleSheet, YellowBox, SafeAreaView, StatusBar } from 'react-native';
 import createStackNavigator from './src/services/navigator';
 
 YellowBox.ignoreWarnings(['Remote debugger']);
@@ -7,14 +7,18 @@ const AppNavigator = createStackNavigator;
 
 export default class App extends React.Component {
   render() {
-    return <AppNavigator style={styles.container} />;
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <AppNavigator />
+      </SafeAreaView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    height: '100%',
+    backgroundColor: '#172E4C'
   }
 });
