fa19d230a720411b0f7b212ce3e508dce1e718a4
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-10-28 18:03:41 +0100

    Fetch players and elo rankings on players screen

--- src/screens/PlayersScreen.js
+++ src/screens/PlayersScreen.js
@@ -1,15 +1,41 @@
 import React, { PureComponent } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Tabs from '../components/tabs/Tabs';
+import { API_URL } from '../config/api';
 
 export default class PlayersScreen extends PureComponent {
   state = {
     selectedIndex: 0,
   };
+
   onPress(selectedIndex) {
     this.setState({
       selectedIndex,
     });
+  }
+
+  async getPlayers(id) {
+    return fetch(`${API_URL}/leagues/${id}/players`)
+      .then(res => res.json())
+      .catch(err => console.log(err));
+  }
+
+  async getEloRanking(id) {
+    return fetch(`${API_URL}/leagues/${id}/elo-ranking`)
+      .then(res => res.json())
+      .catch(err => console.log(err));
+  }
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const league = navigation.getParam('league', '2C');
+    const [{ players }, { players: eloRanking }] = await Promise.all([
+      this.getPlayers(league),
+      this.getEloRanking(league),
+    ]);
+
+    console.table(players);
+    console.table(eloRanking);
   }
 
   render() {
