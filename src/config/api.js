7643854dc180ed33dc0395c764ba91d3fbaa23d8
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-11-09 16:05:35 +0100

    Move API requests to config file

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,25 @@
 export const API_URL = 'https://cbkregio-oost.herokuapp.com';
+
+export const getLeagues = async () => {
+  return fetch(`${API_URL}/leagues`)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+};
+
+export const getLeagueDetail = async id => {
+  return fetch(`${API_URL}/leagues/${id}`)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+};
+
+export const getPlayers = async id => {
+  return fetch(`${API_URL}/leagues/${id}/players`)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+};
+
+export const getEloRanking = async id => {
+  return fetch(`${API_URL}/leagues/${id}/elo-ranking`)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+};
