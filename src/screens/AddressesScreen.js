36b43aa519d88cc774f8ecef83681070714fea3b
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-11-13 00:29:07 +0100

    Finish addresses screen

--- src/screens/AddressesScreen.js
+++ src/screens/AddressesScreen.js
@@ -1,19 +1,29 @@
 import React, { PureComponent } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Clipboard,
+  Alert,
+} from 'react-native';
 import { getAddresses } from '../config/api';
 import { makeCancelable } from '../utils/promise';
 import Swipeout from 'react-native-swipeout';
 import { createOpenLink } from 'react-native-open-maps';
+import ListItem from '../components/list/ListItem';
+import colors from '../utils/colors';
 
 export default class AddressesScreen extends PureComponent {
   state = {
     addresses: [],
     isLoading: true,
+    rowID: null,
   };
 
   componentDidMount() {
     const { navigation } = this.props;
-    const league = navigation.getParam('league', '2C');
+    const league = navigation.getParam('league', null);
 
     this.cancelablePromise = makeCancelable(getAddresses(league));
     this.cancelablePromise.promise
@@ -33,26 +43,124 @@ export default class AddressesScreen extends PureComponent {
   swipeoutBtns(address) {
     return [
       {
-        text: 'Kaart',
+        text: this.renderSwipeoutBtn('Kopieer', 'tel. nr.'),
+        type: 'default',
+        onPress: () =>
+          this.onCopy(
+            address.phone,
+            `Telefoonnummer ${address.club} gekopieerd.`
+          ),
+      },
+      {
+        text: this.renderSwipeoutBtn('Kopieer', 'adres'),
+        type: 'secondary',
+        onPress: () =>
+          this.onCopy(address.address, `Adres ${address.club} gekopieerd.`),
+      },
+      {
+        text: this.renderSwipeoutBtn('Open', 'Kaart'),
+        type: 'primary',
         onPress: createOpenLink({ query: address.address }),
       },
     ];
   }
 
+  onCopy(item, text) {
+    Clipboard.setString(item);
+    Alert.alert(text);
+  }
+
+  renderSwipeoutBtn(title, subtitle) {
+    return (
+      <View style={styles.swipeoutBtn}>
+        <Text
+          numberOfLines={1}
+          ellipsizeMode="clip"
+          style={styles.swipeoutBtnText}
+        >
+          {title}
+        </Text>
+        {subtitle && (
+          <Text
+            numberOfLines={1}
+            ellipsizeMode="clip"
+            style={styles.swipeoutBtnText}
+          >
+            {subtitle}
+          </Text>
+        )}
+      </View>
+    );
+  }
+
   render() {
     return (
-      <>
-        {this.state.addresses.map((address, i) => (
-          <Swipeout key={i} right={this.swipeoutBtns(address)}>
-            <View>
-              <Text>{address.club}</Text>
-              <Text>{address.address}</Text>
-            </View>
-          </Swipeout>
-        ))}
-      </>
+      <View style={styles.screen}>
+        <ScrollView
+          style={styles.scrollView}
+          contentContainerStyle={styles.contentContainerStyle}
+          showsVerticalScrollIndicator={false}
+        >
+          {this.state.addresses.map((address, i) => (
+            <React.Fragment key={i}>
+              {i > 0 && <View style={styles.divider} />}
+              <Swipeout
+                style={[
+                  i === 0 && styles.isFirst,
+                  i === this.state.addresses.length - 1 && styles.isLast,
+                ]}
+                backgroundColor={colors.white}
+                close={this.state.rowID !== i}
+                key={i}
+                rowID={i}
+                right={this.swipeoutBtns(address)}
+                onOpen={(sectionID, rowID) => {
+                  this.setState({ rowID });
+                }}
+              >
+                <ListItem
+                  title={address.club}
+                  subtitle={address.address}
+                  rightTitle={address.place}
+                />
+              </Swipeout>
+            </React.Fragment>
+          ))}
+        </ScrollView>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    height: '100%',
+    paddingHorizontal: 15,
+  },
+  scrollView: {
+    marginTop: 15,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  contentContainerStyle: {
+    paddingBottom: 15,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: colors.mystic,
+  },
+  isFirst: {
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  isLast: {
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  swipeoutBtnText: {
+    textAlign: 'center',
+    fontSize: 12,
+    fontWeight: '500',
+    color: colors.white,
+  },
+});
