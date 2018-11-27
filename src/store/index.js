7d60637a0a66c81bc1c3fd43f42aa1b60651e5e4
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-11-27 01:55:31 +0100

    Persist league detail

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ import rootReducer from '../reducers';
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['leagues', 'addresses'],
+  whitelist: ['leagues', 'addresses', 'leagueDetail'],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middleware = [thunk];
