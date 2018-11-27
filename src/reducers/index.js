b50c5940799679bbc2fd3a6e628c1521b797e613
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-11-27 01:54:35 +0100

    Create league detail reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import leagues from './leagues';
 import addresses from './addresses';
+import leagueDetail from './league-detail';
 import nav from './nav';
 
 export default combineReducers({
   leagues,
   addresses,
+  leagueDetail,
   nav,
 });
---
+++ src/reducers/league-detail.js
@@ -0,0 +1,49 @@
+import {
+  FETCH_LEAGUE_DETAIL_REQUEST,
+  FETCH_LEAGUE_DETAIL_SUCCESS,
+  FETCH_LEAGUE_DETAIL_FAILURE,
+} from '../actions/league-detail';
+
+const initialState = {};
+
+export default function leagueDetailReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_LEAGUE_DETAIL_REQUEST:
+      const leagueState = state[action.league] || {};
+      const { results = [], tables = [] } = leagueState;
+      return {
+        ...state,
+        [action.league]: {
+          ...leagueState,
+          loading: true,
+          error: null,
+          results,
+          tables,
+        },
+      };
+    case FETCH_LEAGUE_DETAIL_SUCCESS:
+      return {
+        ...state,
+        [action.payload.league]: {
+          loading: false,
+          error: null,
+          results: action.payload.results,
+          tables: action.payload.tables,
+          receivedAt: action.receivedAt,
+        },
+      };
+    case FETCH_LEAGUE_DETAIL_FAILURE: {
+      return {
+        ...state,
+        [league]: {
+          loading: false,
+          error: action.payload.error,
+          results: [],
+          tables: [],
+        },
+      };
+    }
+    default:
+      return state;
+  }
+}
