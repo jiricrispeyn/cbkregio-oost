d375947569a01488ec14d76af68f1434acbf83b6
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-11-30 00:03:42 +0100

    Create players reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import leagues from './leagues';
 import addresses from './addresses';
 import leagueDetail from './league-detail';
+import players from './players';
 import nav from './nav';
 
 export default combineReducers({
   leagues,
   addresses,
   leagueDetail,
+  players,
   nav,
 });
---
+++ src/reducers/players.js
@@ -0,0 +1,45 @@
+import {
+  FETCH_PLAYERS_REQUEST,
+  FETCH_PLAYERS_SUCCESS,
+  FETCH_PLAYERS_FAILURE,
+} from '../actions/players';
+
+const initialState = {};
+
+export default function playersReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_PLAYERS_REQUEST:
+      const playersState = state[action.league] || {};
+      return {
+        ...state,
+        [action.league]: {
+          ...playersState,
+          loading: true,
+          error: null,
+          data: playersState.data || [],
+        },
+      };
+    case FETCH_PLAYERS_SUCCESS:
+      return {
+        ...state,
+        [action.payload.league]: {
+          loading: false,
+          error: null,
+          data: action.payload.players,
+          receivedAt: action.receivedAt,
+        },
+      };
+    case FETCH_PLAYERS_FAILURE: {
+      return {
+        ...state,
+        [league]: {
+          loading: false,
+          error: action.payload.error,
+          data: [],
+        },
+      };
+    }
+    default:
+      return state;
+  }
+}
