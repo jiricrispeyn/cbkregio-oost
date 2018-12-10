420f6bd3458ce1d37d2a06a135563ee5a59baecf
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-12-11 00:37:31 +0100

    Add players and eloRanking to persisted store config

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ import rootReducer from '../reducers';
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['leagues', 'addresses', 'leagueDetail'],
+  whitelist: ['leagues', 'leagueDetail', 'addresses', 'players', 'eloRanking'],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middleware = [thunk];
