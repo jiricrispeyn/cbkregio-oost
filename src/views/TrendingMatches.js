4ec8034a25c90b80170ea68db06b22e85822c158
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-12-12 01:42:47 +0100

    Convert TrendingMatches to class and implement fade in for carousel

--- src/views/TrendingMatches.js
+++ src/views/TrendingMatches.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, Text, Dimensions, Platform } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  Platform,
+  Animated,
+} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import { Haptic, Constants } from 'expo';
 import format from 'date-fns/format';
@@ -10,44 +17,65 @@ import TrendingMatch from '../components/matches/TrendingMatch';
 
 const { width } = Dimensions.get('window');
 
-function onSnapToItem() {
-  if (
-    Platform.OS === 'ios' &&
-    parseInt(Constants.platform.ios.systemVersion) >= 10
-  ) {
-    Haptic.selection();
+export default class TrendingMatches extends PureComponent {
+  state = {
+    opacity: new Animated.Value(0),
+  };
+
+  onLayout = () => {
+    if (this.state.opacity === 1) {
+      return;
+    }
+
+    Animated.timing(this.state.opacity, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  onSnapToItem() {
+    if (
+      Platform.OS === 'ios' &&
+      parseInt(Constants.platform.ios.systemVersion) >= 10
+    ) {
+      Haptic.selection();
+    }
+  }
+
+  renderTrendingMatch({ item, index }) {
+    return (
+      <TrendingMatch key={index} match={item} cardStyle={styles.cardStyle} />
+    );
+  }
+
+  render() {
+    const { date, matches } = this.props;
+    const formattedDate = format(date, 'D MMMM YYYY', { locale });
+
+    return (
+      <React.Fragment>
+        <View style={styles.header}>
+          <Text style={styles.title}>Laatste speeldag</Text>
+          <Text style={styles.date}>{formattedDate}</Text>
+        </View>
+        <Animated.View style={{ opacity: this.state.opacity }}>
+          <Carousel
+            data={matches}
+            renderItem={this.renderTrendingMatch}
+            sliderWidth={width}
+            itemWidth={240}
+            useScrollView={true}
+            activeSlideAlignment="start"
+            containerCustomStyle={styles.scrollView}
+            onLayout={this.onLayout}
+            onSnapToItem={this.onSnapToItem}
+          />
+        </Animated.View>
+      </React.Fragment>
+    );
   }
 }
-
-function renderTrendingMatch({ item, index }) {
-  return (
-    <TrendingMatch key={index} match={item} cardStyle={styles.cardStyle} />
-  );
-}
-
-const TrendingMatches = ({ date, matches }) => {
-  const formattedDate = format(date, 'D MMMM YYYY', { locale });
-
-  return (
-    <React.Fragment>
-      <View style={styles.header}>
-        <Text style={styles.title}>Laatste speeldag</Text>
-        <Text style={styles.date}>{formattedDate}</Text>
-      </View>
-      <Carousel
-        data={matches}
-        renderItem={renderTrendingMatch}
-        sliderWidth={width}
-        itemWidth={240}
-        useScrollView={true}
-        activeSlideAlignment="start"
-        containerCustomStyle={styles.scrollView}
-        // onLayout={e => console.log(e)}
-        onSnapToItem={onSnapToItem}
-      />
-    </React.Fragment>
-  );
-};
 
 TrendingMatches.propTypes = {
   date: PropTypes.instanceOf(Date).isRequired,
@@ -79,5 +107,3 @@ const styles = StyleSheet.create({
     marginLeft: 15,
   },
 });
-
-export default TrendingMatches;
