5b152752dc714134cea323bac9f8a31c22433f87
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-06-06 00:56:28 +0200

    Add scoresheet id to nav reducers and actions

--- src/actions/nav.js
+++ src/actions/nav.js
@@ -1,6 +1,12 @@
 export const SET_LEAGUE = 'SET_LEAGUE';
+export const SET_SCORESHEET = 'SET_SCORESHEET';
 
 export const setLeague = league => ({
   type: SET_LEAGUE,
   payload: league,
+});
+
+export const setScoresheet = scoresheet => ({
+  type: SET_SCORESHEET,
+  payload: scoresheet,
 });
--- src/reducers/nav.js
+++ src/reducers/nav.js
@@ -1,11 +1,22 @@
-import { SET_LEAGUE } from '../actions/nav';
+import { SET_LEAGUE, SET_SCORESHEET } from '../actions/nav';
 
-const initialState = null;
+const initialState = {
+  league: null,
+  scoresheet: null,
+};
 
 export default function navReducer(state = initialState, action) {
   switch (action.type) {
     case SET_LEAGUE:
-      return action.payload;
+      return {
+        ...state,
+        league: action.payload,
+      };
+    case SET_SCORESHEET:
+      return {
+        ...state,
+        scoresheet: action.payload,
+      };
     default:
       return state;
   }
